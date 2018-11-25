86fa915d11feb1e66dea31ba0cf0cc2607a00de5
TeNguyen <manhte231@gmail.com>
2018-11-25 21:01:02 +0700

    update model request and bash build

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# build for product
 protoc -I product/  product/product.proto  --go_out=plugins=grpc:product
+# build for user
 protoc -I user/ user/user.proto --go_out=./user
+#build for req
+protoc -I reqres/ reqres/reqres.proto --go_out=./reqres
+
+echo done!
