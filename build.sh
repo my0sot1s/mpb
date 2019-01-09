0e2d8bf7cddcb708590b1d37a4598961b6d0ef92
TeNguyen <manhte231@gmail.com>
2019-01-09 23:54:23 +0700

    submit change

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ protoc common/common.proto -I. --go_out=.
 # build for product
 protoc product/product.proto -I. --go_out=plugins=grpc:.
 # # build for user
-protoc user/user.proto -I. --go_out=plugins=grpc:.
+protoc user/user.proto -I. -I$GOPATH/src/github.com/my0sot1s/mpb --go_out=plugins=grpc:.
 # # build for blog
 protoc blog/blog.proto -I. --go_out=.
 # # build for maker
