4a73fc0d20deb9ae20f259a358e3d3b5d38e7936
Te Nguyen <manhte231@gmail.com>
2019-11-01 23:59:47 +0700

    new version header, add auth, user wsh

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# #build for common
-protoc common/common.proto -I. --go_out=../../../
-# build for product
-protoc product/product.proto -I. --go_out=plugins=grpc:.
 # # build for user
 protoc user/user.proto -I. --go_out=plugins=grpc:.
-# # build for blog
-protoc blog/blog.proto -I. --go_out=plugins=grpc:.
-# # build for maker
-protoc marker/marker.proto -I. --go_out=plugins=grpc:../../../
+
+protoc wsh/wsh.proto -I. --go_out=plugins=grpc:.
+
+protoc auth/auth.proto -I. --go_out=plugins=grpc:.
+
 echo done!
